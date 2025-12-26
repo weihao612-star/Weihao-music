fb07fdf8302c37ce3b0abc2ae0ebd6ea986fabf3
Daniele Golzio <80271815+danielegolzio@users.noreply.github.com>
2025-12-26 01:03:10 +0100

    changing overall look

--- assets/js/hscroll.js
+++ assets/js/hscroll.js
@@ -1,6 +1,17 @@
-const gallery = document.querySelector('.gallery');
+// const gallery = document.querySelector('.gallery');
 
-gallery.addEventListener('wheel', (e) => {
-  e.preventDefault();
-  gallery.scrollLeft += e.deltaY;
-});
+// gallery.addEventListener('wheel', (e) => {
+//   e.preventDefault();
+//   gallery.scrollLeft += e.deltaY;
+// });
+
+const gallery = document.getElementById(".gallery");
+const scrollAmount = gallery.clientWidth * 0.8;
+
+document.querySelector(".scroll-btn.left").onclick = () => {
+  gallery.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+};
+
+document.querySelector(".scroll-btn.right").onclick = () => {
+  gallery.scrollBy({ left: scrollAmount, behavior: "smooth" });
+};
